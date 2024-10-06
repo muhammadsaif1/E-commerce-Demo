9d4cf2181c986bcd5110475bb880c209ffc4672a
Saif <saif.shabir12@gmail.com>
2024-10-06 19:05:18 +0500

    feat/registerUser

---
+++ server/controllers/auth/index.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+
+// register
+const registerUser = async (req, res) => {
+  const { userName, email, password } = req.body;
+
+  try {
+    const hashPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      userName,
+      email,
+      password: hashPassword,
+    });
+    await newUser.save();
+    res.status(200).json({
+      success: true,
+      message: "User created succesfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+// login
+const login = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+// logout
+
+// auth middleware
+
+module.exports = { registerUser };
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/auth", authRouter());
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);

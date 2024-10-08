e3b8532ba405c095a907c57a342392682e0d0bb1
Saif <saif.shabir12@gmail.com>
2024-10-08 17:58:52 +0500

    fix/cors-origin

--- server/middleware/corsHandler.js
+++ server/middleware/corsHandler.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-  origin: "*",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: [
     "Content-Type",

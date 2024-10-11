781126ce879ae63283d059e072571c22b5cd2eb3
Saif <saif.shabir12@gmail.com>
2024-10-11 17:25:08 +0500

    feat/login-User-api

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -7,6 +7,14 @@ const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
 
   try {
+    const checkUser = await User.findOne({ email });
+    if (checkUser) {
+      return res.json({
+        success: false,
+        message: "User already exists, please try different email",
+      });
+    }
+
     const hashPassword = await bcrypt.hash(password, 12);
     const newUser = new User({
       userName,
@@ -28,8 +36,39 @@ const registerUser = async (req, res) => {
 };
 
 // login
-const login = async (req, res) => {
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
   try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return res.json({ success: false, message: "User doesn't exists" });
+    }
+    const checkPassword = await bcrypt.compare(password, checkUser.password);
+    if (!checkPassword) {
+      return res.json({
+        success: false,
+        message: "Your password is incorrect, please try again",
+      });
+    }
+    const token = jwt.sign(
+      {
+        id: checkUser._id,
+        role: checkUser.role,
+        email: checkUser.email,
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+      success: true,
+      message: "Logged In successfully",
+      user: {
+        email: checkUser.email,
+        role: checkUser.role,
+        id: checkUser._id,
+      },
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -43,4 +82,4 @@ const login = async (req, res) => {
 
 // auth middleware
 
-module.exports = { registerUser };
+module.exports = { registerUser, loginUser };
--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
 const connectToDatabase = require("./config/database");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth/index");
+const corsMiddleware = require("./middleware/corsHandler");
 
 connectToDatabase();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(corsMiddleware);
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);

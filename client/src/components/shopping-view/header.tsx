d2931730036b972f2281148000eec239a6c4ac12
Saif <saif.shabir12@gmail.com>
2024-12-07 18:46:03 +0500

    update/header cart quantity badge

--- client/src/components/shopping-view/header.tsx
+++ client/src/components/shopping-view/header.tsx
@@ -93,8 +93,12 @@ function HeaderRightContent() {
           onClick={() => setOpenCartSheet(true)}
           variant={"outline"}
           size={"icon"}
+          className="relative "
         >
           <ShoppingCart className="w-6 h-6" />
+          <span className="absolute top-[-4px] right-[2px] font-bold text-sm">
+            {cartItems?.items?.length || 0}
+          </span>
           <span className="sr-only"> User cart</span>
         </Button>
 
